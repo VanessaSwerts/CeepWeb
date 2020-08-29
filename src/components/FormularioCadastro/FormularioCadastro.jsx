2bd92f6c303f27e0adb973472fc74d8b056d52db
VanessaSwerts <vanessaswerts@gec.inatel.br>
2020-08-29 17:17:51 -0300

    Create project

---
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from "react"
+import "./estilo.css"
+
+export default class FormularioCadastro extends Component {
+    render() {
+        return (
+            <form className="form-cadastro">
+                <input
+                    className="form-cadastro_input"
+                    type="text"
+                    placeholder="Título"
+                />
+                <textarea
+                    className="form-cadastro_input"
+                    rows={15}
+                    placeholder="Escreva sua nota..."
+                />
+                <button className="form-cadastro_input form-cadastro_submit" >
+                    Criar Nota
+                </button>
+            </form>
+        )
+    }
+}
